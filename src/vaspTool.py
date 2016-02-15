23d21447f84224839beda2a826823777a7266e7e
dasahcc <junkai.xue@gmail.com>
2016-02-15 13:42:44 -0800

    Enable --poscar without value assignment

--- src/vaspTool.py
+++ src/vaspTool.py
@@ -6,14 +6,16 @@ from POSCAR import POSCAR
 from io import readStringStream
 
 def executeFunction(args):
-    if args["poscar"]:
+    if not args["poscar"]:
         if args["ATOMS"] is None:
             raise Exception("Invalid argument \"ATOMS\": no atoms found!")
         data = readStringStream()
+
         if args["STRUCTURE"] is not None:
             prototype = open(args["STRUCTURE"], 'r')
             data = prototype.read()
         poscar = POSCAR(data, args["ATOMS"])
+
         poscar.generatePOSCAR(args["OUTPUTPATH"])
     if args["ABCCAR"]:
         xstructure=readStringStream()
@@ -28,7 +30,7 @@ def main():
     # POSCAR PRODUCE COMMAND
     parser.add_argument("-abccar","--ABCCAR", help="Read a POSCAR and convert it as abccar format", default=False, required=False, action="store_true")
 
-    parser.add_argument("-pos", "--poscar", help="Generate POSCAR from prototypes", default=True, required=False)
+    parser.add_argument("-pos", "--poscar", help="Generate POSCAR from prototypes", default=True, required=False, action="store_false")
     parser.add_argument("-sc", "--STRUCTURE", help="Structure used for generating POSCAR", required=False)
     parser.add_argument("-ats", "--ATOMS", help="Atoms to fill in the structure", required=False)
     parser.add_argument("-pwf", "--OUTPUTPATH", help="Output path of the data", default=None, required=False)
